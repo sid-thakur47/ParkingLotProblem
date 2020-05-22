fa65e7ffd43a373a175b2fd4f8dc8e61673a5334
sid-thakur47 <sidthakur258@gmail.com>
2020-05-22 17:08:06 +0530

    ADDED:Test to return true when car is parked

--- src/test/java/com/bl/parkinglot/ParkingLotTest.java
+++ src/test/java/com/bl/parkinglot/ParkingLotTest.java
@@ -1,5 +1,23 @@
 package com.bl.parkinglot;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class ParkingLotTest {
 
+    ParkingLot parkingLot;
+    Object car;
+
+    @Before
+    public void setup() {
+        parkingLot = new ParkingLot();
+        car = new Object();
+    }
+    @Test
+    public void given_Car_WhenParked_ShouldReturnTrue() {
+        boolean isPark = parkingLot.parkCar( car );
+        Assert.assertTrue( isPark );
+    }
 }
+
