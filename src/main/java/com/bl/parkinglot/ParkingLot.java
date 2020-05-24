6317a8fc855f76abd32529a1a2be87748c82b7ce
sid-thakur47 <sidthakur258@gmail.com>
2020-05-24 11:11:16 +0530

    ADDED:Functionality to unPark the car

--- src/main/java/com/bl/parkinglot/ParkingLot.java
+++ src/main/java/com/bl/parkinglot/ParkingLot.java
@@ -1,9 +1,31 @@
+/**********************************************************
+ * @Purpose: Parking and unParking the car
+ * @Author: Siddhesh Thakur
+ * @Date: 22/05/2020
+ **********************************************************/
 package com.bl.parkinglot;
 
 public class ParkingLot {
+
+/**
+ * @param: car Car object
+ **/
+
     private Object car;
+
+    //to park the car
     public boolean parkCar(Object car) {
         this.car = car;
         return true;
     }
+
+    //to unPark the car
+    public boolean unParkCar(Object car) {
+        if (this.car.equals( car )) {
+            this.car = null;
+            return true;
+        }
+        return false;
+    }
 }
+
