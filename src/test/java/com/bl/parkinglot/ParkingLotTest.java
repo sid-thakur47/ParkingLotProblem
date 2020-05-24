c6046ad50733751a5697e92a2e2bbcc0dfb87a20
sid-thakur47 <sidthakur258@gmail.com>
2020-05-24 13:00:50 +0530

    ADDED:test to inform when Parking is full

--- src/test/java/com/bl/parkinglot/ParkingLotTest.java
+++ src/test/java/com/bl/parkinglot/ParkingLotTest.java
@@ -6,31 +6,34 @@ import org.junit.Test;
 
 public class ParkingLotTest {
     ParkingLot parkingLot;
+    ParkingLotOwner owner ;
     Object car;
 
     @Before
     public void setup() {
-        parkingLot = new ParkingLot();
+        parkingLot = new ParkingLot( 1 );
         car = new Object();
+        owner = new ParkingLotOwner();
     }
 
     @Test
-    public void given_Car_WhenParked_ShouldReturnTrue() {
-        boolean isPark = parkingLot.parkCar( car );
+    public void given_Car_WhenParked_ShouldReturnTrue() throws ParkingLotException {
+        parkingLot.park( car );
+        boolean isPark = parkingLot.isCarPark( car );
         Assert.assertTrue( isPark );
     }
 
     @Test
-    public void given_Car_WhenUnParked_ShouldReturnTrue() {
-        parkingLot.parkCar( car );
+    public void given_Car_WhenUnParked_ShouldReturnTrue() throws ParkingLotException {
+        parkingLot.park( car );
         boolean isUnParked = parkingLot.unParkCar( car );
         Assert.assertTrue( isUnParked );
     }
 
     @Test
-    public void given_Car_WhenAlreadyParked_ShouldReturnFalse() {
-        parkingLot.parkCar( car );
-        boolean isPark = parkingLot.parkCar( new Object() );
+    public void given_Car_WhenAlreadyParked_ShouldReturnFalse() throws ParkingLotException {
+        parkingLot.park( car );
+        boolean isPark = parkingLot.isCarPark( new Object() );
         Assert.assertFalse( isPark );
     }
     @Test
@@ -39,10 +42,21 @@ public class ParkingLotTest {
         Assert.assertFalse( isPark );
     }
     @Test
-    public void givenCar_WhenTryToUnParkDifferentVehicle_ShouldReturnFalse() {
-        parkingLot.parkCar(car);
-        boolean isPark = parkingLot.unParkCar(new Object());
-        Assert.assertFalse(isPark);
+    public void givenCar_WhenTryToUnParkDifferentVehicle_ShouldReturnFalse() throws ParkingLotException {
+        parkingLot.park( car );
+        boolean isPark = parkingLot.unParkCar( new Object() );
+        Assert.assertFalse( isPark );
+    }
+    @Test
+    public void given_Car_WhenParkingLotFull_ShouldInformOwner() {
+        parkingLot.registerOwner( owner );
+        try {
+            parkingLot.park( car );
+            parkingLot.park( new Object() );
+        } catch (ParkingLotException e) {
+            boolean checkCapacityFull = owner.isCapacityFull();
+            Assert.assertTrue( checkCapacityFull );
+        }
     }
 }
 
